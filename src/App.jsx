6cb8893482c6197b6bfc6c144f519f5c350f3eae
belenaraceli <primeranobelen3@gmail.com>
2026-04-01 19:48:30 -0300

    Sección transformación agregada con layout responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
+import Transformation from "./components/Transformation";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Services />
+      <Transformation />
     </>
   );
 }
---
+++ src/components/Transformation.jsx
@@ -0,0 +1,39 @@
+import "../styles/transformation.scss";
+
+const Transformation = () => {
+  return (
+    <section className="transformation">
+
+      <div className="transformation__container">
+
+        <div className="transformation__image">
+          <img src="/images/transformacion.jpg" alt="Transformación de vestido" />
+        </div>
+
+        <div className="transformation__text">
+          <h2>Transformación de vestidos</h2>
+
+          <p>
+            ¿Tenés un vestido que te gusta pero no te convence del todo?
+          </p>
+
+          <p>
+            Realizamos modificaciones, ajustes o creamos una versión inspirada
+            en el diseño que soñás, adaptado a tu estilo y medida.
+          </p>
+
+          <p>
+            Cada prenda es trabajada con dedicación para lograr un resultado
+            único y personalizado.
+          </p>
+
+          <button>Consultar</button>
+        </div>
+
+      </div>
+
+    </section>
+  );
+};
+
+export default Transformation;
